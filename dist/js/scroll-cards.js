3e55ac7c11ce50d2b187b7e9250724a2e6d19042
Thabang-R <info@thabangr.co.za>
2020-02-01 18:08:45 +0200

    Modified croll card media query script

--- dist/js/scroll-cards.js
+++ dist/js/scroll-cards.js
@@ -1,33 +1,38 @@
 /* Listen for mouse scroll over scroll cards */
-var mq = window.matchMedia("@media screen and (min-width: 992px)");
-if (mq.matches) {
-  const slider = document.querySelector(".scroll-cards");
-  let isDown = false;
-  let startX;
-  let scrollLeft;
+var screenWidth = window.matchMedia("(min-width: 992px)");
+myFunction(screenWidth); // Call listener function at run time
+screenWidth.addListener(myFunction); // Attach listener function on state changes
+function myFunction(screenWidth) {
+  if (screenWidth.matches) {
+    // If media query matches
+    const slider = document.querySelector(".scroll-cards");
+    let isDown = false;
+    let startX;
+    let scrollLeft;
 
-  slider.addEventListener("mousedown", e => {
-    isDown = true;
-    slider.classList.add("active");
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
-  slider.addEventListener("mouseleave", () => {
-    isDown = false;
-    slider.classList.remove("active");
-  });
-  slider.addEventListener("mouseup", () => {
-    isDown = false;
-    slider.classList.remove("active");
-  });
-  slider.addEventListener("mousemove", e => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 1;
-    slider.scrollLeft = scrollLeft - walk;
-  });
-} else {
+    slider.addEventListener("mousedown", e => {
+      isDown = true;
+      slider.classList.add("active");
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener("mouseleave", () => {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mouseup", () => {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mousemove", e => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 1;
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  } else {
+  }
 }
 
 /* Fade In Scroll Cards */
